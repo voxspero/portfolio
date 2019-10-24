bdaebf13d760132935d85547a8905b69473b40b9
James Bernhardt <jbernhardt7@gmail.com>
2019-10-24 12:54:56 -0400

    add cloud db back to app.js, get it together

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ const   express             = require("express"),
                                 useUnifiedTopology: true,
                                 useFindAndModify: false
                             },
-        dbUrl               = "mongodb://localhost/portfolio";
-
-       // process.env.DATABASEURL ||
+        dbUrl               = process.env.DATABASEURL || "mongodb://localhost/portfolio";
 
 // ROUTES
 
